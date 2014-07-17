d4e23ccdd3e73664e4081e0557e3fcc64949ed8a
Hannah Murray <hlmurray91@gmail.com>
2014-07-17 18:31:24 -0400

    feat(the-charles): add sticky nav and anchor transitions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// Hannah Murray ; The Charles
+
 $(window).load(function() {
   $('.flexslider').flexslider({
     animation: "slide",
@@ -9,3 +11,22 @@ $(window).load(function() {
 $(function() {
     $( "#datepicker" ).datepicker();
   });
+
+$(document).scroll(function(event){
+	if ($(this).scrollTop() > 1) {
+		$(".header-logo").css('display', 'none');
+		$(".header-nav").css('display', 'none');
+		$(".masthead--sticky").css({
+			'display': 'block',
+			'z-index': '99999999',
+			'top':'0'
+		});
+		$(".masthead").addClass("sticky");
+	}
+	else {
+		$(".masthead").removeClass("sticky");
+		$(".masthead--sticky").css('display', 'none');
+		$(".header-logo").css('display', 'inline-block');
+		$(".header-nav").css('display', 'block');
+	}
+});
